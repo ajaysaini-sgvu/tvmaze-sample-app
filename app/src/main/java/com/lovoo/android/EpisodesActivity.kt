8ab19d4ec8179f5295b2c8ee0bf830c7d104cbcd
Ajay Saini <engineer.ajaysaini@gmail.com>
2018-02-22 19:40:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/lovoo/android/EpisodesActivity.kt
@@ -0,0 +1,12 @@
+package com.lovoo.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EpisodesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_episodes)
+    }
+}
